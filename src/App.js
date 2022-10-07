a7cb8fc4b697dc76499a53ef454fe7e85e3191b2
codeversespace <106614727+codeversespace@users.noreply.github.com>
2022-10-07 22:01:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import {v4 as uuidv4} from 'uuid'
+
+console.log(uuidv4())
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
